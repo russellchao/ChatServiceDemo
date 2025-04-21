604817871c58dedb6facb1244fd00daa01ce680d
russellchao <russchaoman@gmail.com>
2025-04-20 22:47:44 -0400

    RestAPI Connectivity endpoint for ChatService

---
+++ src/main/java/com/FlightIQ/ChatService/ChatServiceDemo/ChatController.java
@@ -0,0 +1,20 @@
+package com.FlightIQ.ChatService.ChatServiceDemo;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/api/chat")
+public class ChatController {
+    
+    @GetMapping("/ping")
+    public ResponseEntity<String> ping() {
+
+
+        return ResponseEntity.ok("pong");
+    }
+    
+}
