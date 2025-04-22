0bd49f301f203213d91984f01e418c0abdc49f5a
russellchao <russchaoman@gmail.com>
2025-04-21 22:47:17 -0400

    Cleaned up prompt for metar safety rating

--- src/main/java/com/FlightIQ/ChatService/ChatServiceDemo/ChatController.java
+++ src/main/java/com/FlightIQ/ChatService/ChatServiceDemo/ChatController.java
@@ -15,7 +15,7 @@ public class ChatController {
 
     public ChatController(ChatClient.Builder builder) {
         this.chatClient = builder
-                .defaultSystem("You are an AI assistant who's an expert in translating FAA NOTAMS into English. Pay extra attention to the dates please!")
+                .defaultSystem("You are an AI aviation assistant that helps pilots plan flights. you will be responsible for translating NOTAMS to English as well as summarizing metar reports and forecasts to give pilots a safety outlook.")
                 .build();
     }
 
@@ -29,11 +29,15 @@ public class ChatController {
     }
 
     
-    @GetMapping("/generate")
-    public ResponseEntity<String> generate(@RequestParam(value = "message") String message) {
+    @GetMapping("/translatenotam")
+    public ResponseEntity<String> translateNotam(@RequestParam(value = "NOTAM") String NOTAM) {
+
+        String promptStr = "You are an expert at translating NOTAMS. Please be as concise as you can given the NOTAM, say 3 lines max, and also please be extra cautious of dates.";
+        String fullPrompt = promptStr + NOTAM; 
+
         try {
             String response = chatClient.prompt()
-                    .user(message)
+                    .user(fullPrompt)
                     .call()
                     .content()
                     .toString();
@@ -53,11 +57,11 @@ public class ChatController {
         String endpoint = apiUrl.replace("{ICAO}", ICAO);
         RestTemplate restTemplate = new RestTemplate(); 
         String apiResponseJSON = restTemplate.getForObject(endpoint, String.class);
-        String promptStr = "You are an expert single engine plane pilot. rate this METAR on a saftey scale of 1-10 with 10 being the safest to fly. be concise.";
+        String promptStr = "You are an expert single engine plane pilot. rate this METAR on a saftey scale of 1-10 with 10 being the safest to fly. be concise. Also, if there's hazardous conditions like thunderstorms, lower the rating significantly.";
         String fullPrompt = promptStr + apiResponseJSON;
        
-
         try {
+
             String response = chatClient.prompt()
                     .user(fullPrompt)
                     .call()
