a63817ecff925e9d72cf9de22ecaedec0d987793
russellchao <russchaoman@gmail.com>
2025-04-21 22:20:01 -0400

    Added ollama prompt for metar safety

--- src/main/java/com/FlightIQ/ChatService/ChatServiceDemo/ChatController.java
+++ src/main/java/com/FlightIQ/ChatService/ChatServiceDemo/ChatController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 
 @RestController
@@ -17,6 +18,7 @@ public class ChatController {
                 .defaultSystem("You are an AI assistant who's an expert in translating FAA NOTAMS into English. Pay extra attention to the dates please!")
                 .build();
     }
+
 
     private final ChatClient chatClient;
 
@@ -42,5 +44,33 @@ public class ChatController {
             return ResponseEntity.internalServerError().body("An error occurred: " + e.getMessage());
         }
     }
-    
+
+
+    @GetMapping("/ratemetarsafety")
+    public ResponseEntity<String> RateMETARSafety(@RequestParam(value = "ICAO") String ICAO) {
+
+        String apiUrl = "https://wx-svc-x86-272565453292.us-central1.run.app/api/v1/getAirportWeather?airportCode={ICAO}";
+        String endpoint = apiUrl.replace("{ICAO}", ICAO);
+        RestTemplate restTemplate = new RestTemplate(); 
+        String apiResponseJSON = restTemplate.getForObject(endpoint, String.class);
+        String promptStr = "You are an expert single engine plane pilot. rate this METAR on a saftey scale of 1-10 with 10 being the safest to fly. be concise.";
+        String fullPrompt = promptStr + apiResponseJSON;
+       
+
+        try {
+            String response = chatClient.prompt()
+                    .user(fullPrompt)
+                    .call()
+                    .content()
+                    .toString();
+
+            return ResponseEntity.ok(response);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.internalServerError().body("An error occurred: " + e.getMessage());
+        }
+    }
+
 }
+
+    
